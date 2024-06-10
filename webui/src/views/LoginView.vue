715c9fa169144b1dd9ec9b9e81cca9332598ea39
Kevin <huang.1993737@studenti.uniroma1.it>
2024-06-10 18:58:02 +0200

    LoginView.vue overflow hidden

--- webui/src/views/LoginView.vue
+++ webui/src/views/LoginView.vue
@@ -44,53 +44,64 @@ export default {
     </div>
   </div>
 
-    <div>
-
-      <div class="site-name" style="top: 15%">
-        <h1 class="h1">WASAPhoto</h1>
-      </div>
-
-      <div class="d-flex justify-content-center position-absolute" style="top: 40%; left: 0; width: 100%; height: 100%;">
-        <div class="justify-content-between flex-wrap flex-md-nowrap align-items-center">
-          <h2 class="h2">Welcome to WASAPhoto</h2>
-          <h2 class="h2 text-center" v-if="username">{{ username }}</h2>
-          <div class="border-bottom"></div>
-        </div>
-      </div>
-      <div class="d-flex justify-content-center position-absolute" style="top: 50%; left: 0; width: 100%; height: 100%; padding-top: .5rem" >
-        <form @submit.prevent="doLogin">
-          <div>
-            <input type="text" id="username" v-model="username" class="form-control"
-                   placeholder="What's your name?" aria-label="Recipient's username" aria-describedby="basic-addon2" autocomplete="off">
-            <div class="text-center">
-              <button class="btn" type="submit">
-                <div class="d-flex justify-content-between">
-                  <svg class="feather"><use href="/feather-sprite-v4.29.0.svg#key"/></svg>
-                  <span>Login</span>
-                </div>
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
+  <div>
+    <div class="site-name" style="top: 15%">
+      <h1 class="h1">WASAPhoto</h1>
     </div>
+
+    <div class="d-flex justify-content-center position-absolute" style="top: 40%; left: 0; width: 100%; height: 100%;">
+      <div class="justify-content-between flex-wrap flex-md-nowrap align-items-center">
+        <h2 class="h2">Welcome to WASAPhoto</h2>
+        <h2 class="h2 text-center" v-if="username">{{ username }}</h2>
+      </div>
+
+    </div>
+    <div class="d-flex justify-content-center position-absolute" style="top: 50%; left: 0; width: 100%; height: 100%; padding-top: .5rem" >
+      <form @submit.prevent="doLogin">
+        <div>
+          <input type="text" id="username" v-model="username" class="form-control"
+                 placeholder="What's your name?" aria-label="Recipient's username" aria-describedby="basic-addon2" autocomplete="off">
+          <div class="text-center" style="padding-top: .5rem">
+            <button class="btn" type="submit">
+              <div class="d-flex justify-content-between">
+                <svg class="feather"><use href="/feather-sprite-v4.29.0.svg#key"/></svg>
+                <span>Login</span>
+              </div>
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
-  <style>
-    .site-name {
-      display: flex;
-      justify-content: center;
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      left: 0;
-      font-size: 3rem;
-      font-weight: bold;
-      text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
-    }
+<style>
+body, html {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  overflow: hidden;
+}
 
-    .site-name h1 {
-      font-size: 6rem;
-    }
+.site-name {
+  display: flex;
+  justify-content: center;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  font-size: 3rem;
+  font-weight: bold;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+}
 
-  </style>
+.site-name h1 {
+  font-size: 6rem;
+}
+
+form {
+  width: 100%;
+  max-width: 400px;
+}
+
+</style>
